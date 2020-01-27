da0ab929e832f11a4a7e8bffbfe85237b1e6991b
Kevin Mueller <>
2020-01-27 12:04:13 +0100

    Implemented ECEFToLLA

--- src/ecef_uilts.ts
+++ src/ecef_uilts.ts
@@ -40,7 +40,7 @@ export interface LLAPoint {
  * @param {LLAPoint} llaPt
  * @returns {ECEFPoint}
  */
-export function LLAToECEF (llaPt: LLAPoint): ECEFPoint {
+export function LLAToECEF(llaPt: LLAPoint): ECEFPoint {
     const latRad = utils.degToRad(llaPt.lat);
     const lonRad = utils.degToRad(llaPt.lon);
     const N = radiusOfCurvature(latRad);
@@ -49,24 +49,41 @@ export function LLAToECEF (llaPt: LLAPoint): ECEFPoint {
     const y = (N + llaPt.alt) * Math.cos(latRad) * Math.sin(lonRad);
     const z = (N * (Math.pow(constants.POLAR_RADIUS, 2) / Math.pow(constants.RADIUS, 2)) + llaPt.alt) * Math.sin(latRad);
 
-    const ecefPt: ECEFPoint = {x: x, y: y, z: z};
+    const ecefPt: ECEFPoint = { x: x, y: y, z: z };
     return ecefPt;
 }
 
 /**
  * Projects an ECEF x, y, z point to lat, lon, alt coordinates
+ * This is an implementation of the closed set formula described in section 2.2
+ * of the paper.
  *
  * @param {ECEFPoint} ecefPt
  * @returns {LLAPoint}
  */
-export function ECEFToLLA (ecefPt: ECEFPoint): LLAPoint {
-    const llaPt: LLAPoint = {lat: 0, lon: 0, alt: 0};
+export function ECEFToLLA(ecefPt: ECEFPoint): LLAPoint {
+    // Calculate auxiliary values
+    const p = Math.sqrt((ecefPt.x * ecefPt.x) + (ecefPt.y * ecefPt.y));
+    const theta = Math.atan((ecefPt.z * constants.RADIUS) / (p * constants.POLAR_RADIUS));
+
+    // Calculate longitude
+    const lonRad = Math.atan(ecefPt.y / ecefPt.x);
+
+    // Calculate latitude
+    const numerator = ecefPt.z + Math.pow(constants.SECOND_ECCENTRICITY, 2) * constants.POLAR_RADIUS * Math.pow(Math.sin(theta), 3);
+    const denominator = p - Math.pow(constants.FIRST_ECCENTRICITY, 2) * constants.RADIUS * Math.pow(Math.cos(theta), 3);
+    const latRad = Math.atan(numerator / denominator);
+
+    // Calculate altitude above the ellipsoid
+    const N = radiusOfCurvature(latRad);
+    const alt = (p / Math.cos(latRad)) - N;
+
+    // Convert lat and lon to degrees and return the LLA point
+    const llaPt: LLAPoint = { lat: utils.radToDeg(latRad), lon: utils.radToDeg(lonRad), alt: alt };
     return llaPt;
 }
 
-function radiusOfCurvature(lat:number): number {
-    const radiusOfCurvature: number = constants.RADIUS / Math.sqrt(1 - (Math.pow(constants.FIRST_ECCENTRICITY, 2) * Math.pow(Math.sin(lat), 2)))
-
+function radiusOfCurvature(lat: number): number {
+    const radiusOfCurvature = constants.RADIUS / Math.sqrt(1 - (Math.pow(constants.FIRST_ECCENTRICITY, 2) * Math.pow(Math.sin(lat), 2)));
     return radiusOfCurvature;
 }
-
